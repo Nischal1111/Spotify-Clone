f6c1ec289f2a9107ac7f8dc7f7c2e9da9608f2d2
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2023-07-20 23:22:22 +0545

    Updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { StateContext } from './components/StateProvider.Js';
+import { initialState } from './reducer';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StateContext.Provider initialState={initialState}>
     <App />
+    </StateContext.Provider>
   </React.StrictMode>
 );

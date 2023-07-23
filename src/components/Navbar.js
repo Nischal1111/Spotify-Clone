06ce246800e60e149c6d55232ea2ef2c5e56ca4f
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2023-07-23 22:51:01 +0545

    Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import "../css/navbar.css"
+import SearchIcon from '@mui/icons-material/Search'
 
 export default function Navbar() {
   return (
     <div>
-      Navbar
+      <div className='header'>
+        <div className='header--left'>
+            <SearchIcon/>
+            <input type='text' placeholder='Search for Artists, Songs or Podcasts...'></input>
+        </div>
+        </div>
     </div>
   )
 }

c09202e12a371adeafcd7d2355c6bf8d6005d7aa
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2023-08-09 10:32:10 +0545

    Updated

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {BsFillPlayCircleFill,BsFillPauseCircleFill,BsShuffle} from "react-icons/bs";
 import {CgPlayTrackNext,CgPlayTrackPrev} from "react-icons/cg"
 import {FiRepeat} from "react-icons/fi"
+import { useStateProvider } from './sp';
 
 export default function Controls() {
+    const [{token},dispatch] = useStateProvider();
+    useEffect(() => {
+    const getPlayState = async () => {
+      const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json',
+        },
+      });
+        const {item}=response.name
+      dispatch({ type: reducerCases.SET_PLAYER_STATE, PlayerState });
+    };
+    getPlayState();
+  }, [token, dispatch]);
+
   return (
     <div>
       <div className='shuffle'>

86ab93661b79f56fc36c0b8fdd7471fcd086ab58
Nischal Neupane <102789731+Nischal1111@users.noreply.github.com>
2023-08-18 08:02:52 +0545

    Updated

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,32 +1,30 @@
 import React, { useEffect } from 'react'
+import axios from 'axios';
+import { reducerCases } from './constants';
 import {BsFillPlayCircleFill,BsFillPauseCircleFill,BsShuffle} from "react-icons/bs";
 import {CgPlayTrackNext,CgPlayTrackPrev} from "react-icons/cg"
 import {FiRepeat} from "react-icons/fi"
 import { useStateProvider } from './sp';
 
 export default function Controls() {
-    const [{token},dispatch] = useStateProvider();
-    useEffect(() => {
-    const getPlayState = async () => {
-      const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
+    const [{token,PlayerState},dispatch] = useStateProvider();
+    const changetrack =async(type)=>{
+      const response = await axios.get(`https://api.spotify.com/v1/me/player/${type}`, {
         headers: {
           Authorization: 'Bearer ' + token,
           'Content-Type': 'application/json',
         },
       });
-        const {item}=response.name
-      dispatch({ type: reducerCases.SET_PLAYER_STATE, PlayerState });
-    };
-    getPlayState();
-  }, [token, dispatch]);
+      dispatch({ type: reducerCases.PlayerState, PlayerState});
+    }
 
   return (
-    <div>
+    <div style={{display:"flex",alignItems:"center",justifyContent:"center"}}>
       <div className='shuffle'>
         <BsShuffle/>
       </div>
       <div className='prev--'>
-        <CgPlayTrackPrev/>
+        <CgPlayTrackPrev onClick={changetrack(type)}/>
       </div>
       <div className='play--'>
         {PlayerState ? <BsFillPauseCircleFill/>: <BsFillPlayCircleFill/>}
